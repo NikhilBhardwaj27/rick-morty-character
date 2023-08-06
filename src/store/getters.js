91b25192e8d21af8cb612950667ac86b74bc440a
rajatbhatia1998 <rajat.bhatia859@gmail.com>
2023-08-06 21:06:45 +0530

    getters push

--- src/store/getters.js
+++ src/store/getters.js
@@ -47,13 +47,55 @@ export const coreFilterLogic = (result1, filterValue) => {
   });
 };
 
+export const newLogic = (filters,data)=>{
+ 
+  let speciesArray = data.filter(d=>{
+    if(filters.includes(d.species)){
+      return d
+    }else if(d.species!=="Human" && d.species!=="Mytholog"  && filters.includes("Other Species...")){
+      return d
+    }else{
+      return null
+    }
+  
+  })
+
+  let pastArray = speciesArray.length > 0 ? speciesArray : data
+  let genderArray = pastArray.filter(d=>filters.includes(d.gender))
+
+
+
+  let pastArray2 = genderArray.length > 0 ? genderArray : (pastArray.length > 0 ? pastArray : data ) 
+  let originArray = pastArray2.filter(d=>{
+    if(filters.includes(d.origin?.name)){
+      return d
+    }else if(d.origin?.name!=="unknown" && d.origin?.name!=="Post-Apocalyptic Earth" && d.origin?.name!=="Nupita 4" && filters.includes("Other Origins...")){
+      return d
+    }else{
+      return null
+    }
+  })
+    
+    
+    
+  
+  if(originArray.length > 0 ){
+    return originArray
+  }else{
+    return pastArray2
+  }
+ 
+}
 export const filtersLogic = (state) => {
   if (state.filteredValues.length == 0) return state.rickMorty.result;
-  let result1 = state.rickMorty.result.slice();let result2;
+  let result1 = state.rickMorty.result.slice();
+  let result2;
   let localFilterValues = state.filteredValues.slice();
-  localFilterValues.forEach((filterValue) => {
-    result2 = coreFilterLogic(result1, filterValue);
-    result1 = result2;
-  });
+  // localFilterValues.forEach((filterValue) => {
+  //   result2 = coreFilterLogic(result1, filterValue);
+  //   result1 = [...result2,...result1];
+  // });
+ 
+  result2 = newLogic(localFilterValues,result1)
   return result2;
 };
